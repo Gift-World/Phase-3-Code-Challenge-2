e9665717328c3270414e006b9facc413839fcc23
charles <charles.angila@student.moringaschool.com>
2024-09-07 23:04:56 +0300

    "Added setter for 'name' property in Customer class with input validation"

--- Coffee_shop.py
+++ Coffee_shop.py
@@ -4,7 +4,14 @@ class Customer:
         
     @property
     def name(self):
-        return self._name    
+        return self._name  
+    
+    @name.setter
+    def name(self,value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:    
+            raise ValueError("Name must be a string between 1 and 15 characters")  
         
     
     
