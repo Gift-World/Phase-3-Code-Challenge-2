c959a8251402403e318bfe4c0fee3e3edbc4f46b
charles <charles.angila@student.moringaschool.com>
2024-09-08 19:13:26 +0300

    "Added properties, methods, and validation to Coffee and Customer classes"

--- customer.py
+++ customer.py
@@ -1,8 +1,14 @@
+
+
 class Customer:
     def __init__(self,name):
         self.name = name
+        self._orders = []
+        
         
     @property
+    
+    #Gets the customers name
     def name(self):
         return self._name  
     
@@ -12,4 +18,30 @@ class Customer:
             self._name = value
         else:    
             raise ValueError("Name must be a string between 1 and 15 characters")  
+        
+    def orders(self):
+        
+        #Returns a list of orders for customer
+        return self._orders
+    
+   # Adding an order to the customer's list
+
+    def add_order(self, order):
+        from order import Order
+        if not isinstance(order,Order):
+            raise TypeError("Order must be an instance of the Order class")   
+        self._orders.append(order) 
+        
+        
+    def coffee(self):
+        from coffee import Coffee
+        
+        #Returns a unique set of coffee objects related to customer's orders
+        return {order.coffee for order in self._orders if isinstance(order.coffee,Coffee)} 
+        
+        #Creating new orders
+    def create_order(self,coffee,price):
+        from order import Order
+        new_order = Order(self,coffee,price)
+        return new_order     #Returns the new order     
         
